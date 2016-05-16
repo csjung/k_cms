7b21ba860b66e2df9983317235377de4fa0a0b69
csjung <csjung0061@gmail.com>
2016-05-16 13:13:32 +0900

    first source

---
+++ src/main/java/com/korea/csjung/KCmsApplication.java
@@ -0,0 +1,12 @@
+package com.korea.csjung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KCmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/korea/csjung/KCmsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.korea.csjung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KCmsApplication.class)
+@WebAppConfiguration
+public class KCmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
